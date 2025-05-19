591430cc4ba75bcca2c4e9e9af697f504b8b5adf
Ian Ralphs <29889314+ian-ralphs@users.noreply.github.com>
2025-05-19 15:53:36 +0100

    bump scala version to work with Java 21

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ BROWSER=$1
 ENVIRONMENT=$2
 
 sbt scalafmtCheckAll scalafmtSbtCheck
-sbt -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner" testReport
