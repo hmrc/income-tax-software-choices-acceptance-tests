5caf88638750e868664e70975a2d98815e3e3b09
paddynski-opencast <128393110+paddynski-opencast@users.noreply.github.com>
2025-07-11 14:10:19 +0100

    [ITSASU-4125] Update filters

--- src/test/scala/uk/gov/hmrc/test/ui/pages/SoftwareResultsPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/SoftwareResultsPage.scala
@@ -31,33 +31,13 @@ object SoftwareResultsPage extends BasePage {
   val softwareVendorList: String = "software-vendor-list"
 
   val toFilterId: Map[String, String] = Map(
-    "Free version"                         -> "free-version-filter",
-    "Record keeping"                       -> "record-keeping-filter",
-    "Bridging"                             -> "bridging-filter",
-    "VAT"                                  -> "vat-filter",
-    "Blindness or impaired vision"         -> "visual-filter",
-    "Deafness or impaired hearing"         -> "hearing-filter",
-    "Motor or physical difficulties"       -> "motor-filter",
-    "Cognitive impairments"                -> "cognitive-filter",
-    "6th April to 5th April"               -> "standard-update-periods-filter",
-    "1st April to 31st March"              -> "calendar-update-periods-filter",
-    "Self-employment"                      -> "sole-trader-filter",
-    "UK property"                          -> "uk-property-filter",
-    "Foreign property"                     -> "overseas-property-filter",
-    "Construction industry scheme (CIS)"   -> "construction-industry-scheme-filter",
-    "Capital Gains Tax"                    -> "capital-gains-tax-filter",
-    "Employment"                           -> "employment-filter",
-    "Foreign Income"                       -> "foreign-income-filter",
-    "UK Dividends"                         -> "uk-dividends-filter",
-    "UK interest"                          -> "uk-interest-filter",
-    "Charitable giving"                    -> "charitable-giving-filter",
-    "High Income Child Benefit Charge"     -> "high-income-child-benefit-charge-filter",
-    "Student loans"                        -> "student-loans-filter",
-    "Voluntary Class 2 National Insurance" -> "voluntary-class-2-national-insurance-filter",
-    "State pension income"                 -> "state-pension-income-filter",
-    "Private pension income"               -> "private-pension-income-filter",
-    "Payments into a private pension"      -> "payments-into-a-private-pension-filter",
-    "Marriage Allowance"                   -> "marriage-allowance-filter"
+    "Free version"                   -> "free-version-filter",
+    "Bridging"                       -> "bridging-filter",
+    "VAT"                            -> "vat-filter",
+    "Blindness or impaired vision"   -> "visual-filter",
+    "Deafness or impaired hearing"   -> "hearing-filter",
+    "Motor or physical difficulties" -> "motor-filter",
+    "Cognitive impairments"          -> "cognitive-filter"
   )
 
   def loadPage: this.type = {
