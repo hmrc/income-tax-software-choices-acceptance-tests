f73b148fcc7be56bd66dd00333a89d045b286639
navithamuniraju-cpu <261942424+navithamuniraju-cpu@users.noreply.github.com>
2026-05-13 16:00:45 +0100

    ISC-604: Adding test case for Unguided Journey

---
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/UnguidedJourneySpec.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.test.ui.specs
+
+import uk.gov.hmrc.test.ui.pages.*
+import uk.gov.hmrc.test.ui.pages.HowYouFindSoftwarePage.JourneyType.Find
+import uk.gov.hmrc.test.ui.pages.UserTypePage.UserType
+
+class UnguidedJourneySpec extends BaseSpec {
+
+  Feature("Unguided journey") {
+
+    Scenario("User selects View All journey when Check feature switch is enabled") {
+
+      Given("I enable the Check Journey feature switch")
+      FeatureSwitchPage.setFeatureSwitches(Seq("CheckJourney"))
+
+      When("I navigate to the index route")
+      IndexPage.goTo()
+
+      And("I select the 'Show a list of all' option and click continue")
+      HowYouFindSoftwarePage.selectJourney(Find)
+
+      And("I select 'As an agent' and click continue")
+      UserTypePage.selectUserType(UserType.Agent)
+
+      Then("On the software results page I select a preference filter")
+      SoftwareResultsPage.onPage(isAgent = true)
+      SoftwareResultsPage.checkVendorDisplayed(vendor = "01", expected = true)
+      SoftwareResultsPage.selectPreferenceFilters(Seq("Free version"))
+      SoftwareResultsPage.onPage(isAgent = true)
+      SoftwareResultsPage.checkVendorDisplayed(vendor = "01", expected = false)
+    }
+  }
+}
