4bad8f10263c4015deceb64a1fd53cd6a5a68b59
navithamuniraju-cpu <261942424+navithamuniraju-cpu@users.noreply.github.com>
2026-06-02 13:50:08 +0100

    ISC-465: Removing the coomented lines as per the review comment

--- src/test/scala/uk/gov/hmrc/test/ui/specs/CheckJourneySpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/CheckJourneySpec.scala
@@ -137,11 +137,6 @@ class CheckJourneySpec extends BaseSpec {
 
       And("On the CYA page I click continue")
       CheckYourAnswersPage.onPage()
-      //CheckYourAnswersPage.submitPage()
-
-      /* To Do - Update this to redirect to the correct page once the quarterly updates only page is implemented. */
-      //Then("I am on the compatible for quarterly updates page")
-
     }
 
     Scenario("User checks not compatible software") {
