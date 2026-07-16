5577bc9046b85d43b332ca1cd6f4981abd5de16b
josenewmano <98953155+Josenewmano@users.noreply.github.com>
2026-07-16 11:26:32 +0100

    Remove defunct test

--- src/test/scala/uk/gov/hmrc/test/ui/specs/SoleTraderOrLandlordSpec.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/specs/SoleTraderOrLandlordSpec.scala
@@ -152,37 +152,6 @@ class SoleTraderOrLandlordSpec extends BaseSpec {
       SessionExpiredPage.onPage()
     }
 
-    Scenario("User selects Find journey") {
-
-      Given("I navigate to the index route")
-      IndexPage.goTo()
-
-      And("I select the 'Find' option and click continue")
-      HowYouFindSoftwarePage.selectJourney(Find)
-
-      And("I select 'As a sole trader or landlord' and click continue")
-      UserTypePage.selectUserType(SoleTraderOrLandlord)
-
-      When("I select my business income sources and click continue")
-      BusinessIncomePage.selectBusinessIncomes(Seq(SelfEmployment, UKProperty, ForeignProperty))
-
-      And("I select my additional income sources and click continue")
-      AdditionalIncomePage.selectAdditionalIncomes(Seq(AdditionalIncome.NoneOfThese))
-
-      And("I select my other income sources and click continue")
-      OtherItemsPage.selectOtherItems(Seq(OtherItems.NoneOfThese))
-
-      And("I select my accounting period and click continue")
-      AccountingPeriodPage.selectAccountingPeriod(SixthToFifth)
-
-      And("On the CYA page I click continue")
-      CheckYourAnswersPage.onPage()
-      CheckYourAnswersPage.submitPage()
-
-      Then("I am on the software results page")
-      SoftwareResultsPage.onPage(isUnguided = false)
-    }
-
     Scenario("User changes user answers") {
 
       Given("I navigate to the index route")
